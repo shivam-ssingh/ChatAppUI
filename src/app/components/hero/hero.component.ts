bb62fc8d3f44f3c864b467ba03c7cfa8a592cd89
Shivam Singh <shivamss17.98@gmail.com>
2025-03-23 16:23:07 +0000

    Cleanup and making the routes work

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrl: './hero.component.css',
 })
-export class HeroComponent {
-
-}
+export class HeroComponent {}
